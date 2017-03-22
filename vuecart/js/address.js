2ba3aad279fac8e61520e92cfed1ecc62bdc437b
lzsfe <1690278322@qq.com>
2017-03-22 11:23:05 +0800

    vue购物车1.0

--- vuecart/js/address.js
+++ vuecart/js/address.js
@@ -1,6 +1,8 @@
 new Vue({
 	el:"#address",
 	data:{
+		shippingMethod:1,
+		chooseItem:0,
 		limitList:3,
 		addressList:[
 			{
@@ -12,25 +14,25 @@ new Vue({
 				isDefault:true
 			},
 			{
-				addressID:'10001',
+				addressID:'10002',
 				userName:'李忠胜',
-				streetName:'四川省成都市郫县红光镇西华大学',
+				streetName:'贵州省安顺市第一高级中学',
 				postCode:'610039',
 				tel:'18228075165',
 				isDefault:false
 			},
 			{
-				addressID:'10001',
+				addressID:'10003',
 				userName:'李忠胜',
-				streetName:'四川省成都市郫县红光镇西华大学',
+				streetName:'贵州省平坝县白云镇白云村',
 				postCode:'610039',
 				tel:'18228075165',
 				isDefault:false
 			},
 			{
-				addressID:'10001',
+				addressID:'10004',
 				userName:'李忠胜',
-				streetName:'四川省成都市郫县红光镇西华大学',
+				streetName:'四川省成都市郫县红光镇西华大学老宿舍区一舍203',
 				postCode:'610039',
 				tel:'18228075165',
 				isDefault:false
@@ -51,6 +53,15 @@ new Vue({
 		},
 		listMore:function(){
 			this.limitList = this.addressList.length;
+		},
+		setDefault:function(address){
+			this.addressList.forEach(function(item,index){
+				if (address.addressID == item.addressID) {
+					address.isDefault = true;
+				}else{
+					item.isDefault = false;
+				}
+			})
 		}
 	},
 	computed:{
